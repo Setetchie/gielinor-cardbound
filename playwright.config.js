185bd11d6ecdca2343e9b2c0953bbe40eb972ee3
Setetchie <74015116+Setetchie@users.noreply.github.com>
2026-08-14 17:29:21 -0600

    Configure Cardbound browser regression tests

---
+++ playwright.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require('@playwright/test');
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  use: { baseURL: 'http://127.0.0.1:4173', viewport: { width: 390, height: 844 } },
+  webServer: { command: 'python3 -m http.server 4173', port: 4173, reuseExistingServer: false },
+  reporter: [['list']]
+});
